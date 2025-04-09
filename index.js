47c71c49dcd9a1ba9e19c1a4734dc58f19b0660b
daniel12610 <davega4@hotmail.com>
2025-04-09 16:44:21 -0500

    img handler render

--- index.js
+++ index.js
@@ -14,20 +14,31 @@ app.get("/", (req, res) => {
   res.send("MOABM Server is running.");
 });
 
+const path = require("path");
+const fs = require("fs");
+
 app.post("/upload", upload.single("file"), (req, res) => {
   if (!req.file) return res.status(400).json({ message: "No file uploaded" });
 
-  const fileUrl = `/uploads/${req.file.filename}`;
-  res.json({ 
+  const ext = path.extname(req.file.originalname); // .jpg, .png, etc.
+  const newFileName = `${req.file.filename}${ext}`;
+  const oldPath = req.file.path;
+  const newPath = path.join("uploads", newFileName);
+
+  fs.renameSync(oldPath, newPath); // renombra con extensión
+
+  const fileUrl = `/uploads/${newFileName}`;
+
+  res.json({
     message: `${req.file.originalname} uploaded successfully!`,
     fileUrl,
-    originalName: req.file.originalname
+    originalName: req.file.originalname,
   });
 
   io.emit("chatMessage", {
     username: "Server",
     text: `uploaded file: ${req.file.originalname}`,
-    fileUrl
+    fileUrl,
   });
 });
 
